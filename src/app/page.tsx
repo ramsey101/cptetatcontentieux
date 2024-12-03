636443147d28267e7a5a7cd72be9d0af76775045
ramsey101 <10009245j@gmail.com>
2024-12-03 17:57:09 +0000

    initialize pdf 🫳

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,31 +4,32 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-row gap-8 row-start-2 items-center sm:items-start">
-      <Link href="/acceuil">
-          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Formulaire de réclamation</button>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Link href="/acceuil">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Formulaire de réclamation cel</button>
         </Link>
-        <Link href="/page2">
-          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Page 2</button>
+        <Link href="/recepisse">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Recepisse de reception de reclamation</button>
         </Link>
-        <Link href="/page3">
-          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Page 3</button>
+        <Link href="/radiation">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">fiche récapitulative des radiations à opérer </button>
         </Link>
-        <Link href="/page4">
-          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Page 4</button>
+        <Link href="/reclamation-recues">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">liste récapitulative des réclamations reçues </button>
         </Link>
-        <Link href="/page5">
-          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Page 5</button>
+        <Link href="/reclamation-recues-day">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">liste journaliere des réclamations reçues </button>
         </Link>
-        <Link href="/page6">
-          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Page 6</button>
+        <Link href="/reintegration-omis">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">réintégration d électeur rejeté par erreur de la lep (omis)</button>
         </Link>
-        <Link href="/page7">
-          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Page 7</button>
+        <Link href="/reintegration-motif-erroné">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">fiche individuelle inscription réintégration d électeur rejeté par erreur de la lep (motif de rejet avéré erroné)</button>
         </Link>
-     
+        <Link href="/correction-mention-personnelles">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">fiche individuelle de correction de mention personnelles</button>
+        </Link>
       </main>
-   
     </div>
   );
 }
---
+++ src/app/reintegration-omis/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import FormReclamation  from "@/composants/formreclamation";
+import { DonneesForm } from "@/types/donnesform";
+
+const mesdatas: DonneesForm = {
+  titre: "RECEPISSE DE DEPOT DE RECLAMATION",
+  region: "AGNEBY TIASSA",
+  departement: "TIASSALE",
+  cel: " CESP TIASSALE",
+  ordre: "ORDRE 1",
+  dateform: "17/12/2022",
+  nomlv: "M. FABIO",
+  typerec: "INSCRIPTION OU RECTIFICATION OU RADIATION",
+  datelieunaiss: "17/12/2022",
+  nomprenom: "M. FABIO",
+  nomprenomspere: "M. FABIO",
+  nomprenomsmere: "M. FABIO",
+  nomprenomspv: "M. FABIO",
+  nomprenomsperepv: "M. FABIO",
+  nomprenomsmerepv: "M. FABIO",
+  statutpv: "OMIS SUR LA LEP ou RECENSE",
+  numelecteur: "1",
+  numelecteurpv: "10555eud",
+  piecesjointes: ["Acte de naissance", "Certificat de nationalité", "Acte de mariage", "Acte de naissance de l'enfant"],
+  refrecep: "10554",
+  argument: "M. FABIO",
+  datelieunaisspv: "17/12/2022",
+  decisioncel: "Irrecevabilité ou Acceptation ou Rejet ou Mise en instance(sursis à statuer)",
+  argumentdec: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+};
+
+export default function Page() {
+  return (<>
+  <div className="flex items-center justify-center min-h-screen">
+    <FormReclamation mesdatas={mesdatas} />
+  </div>
+ 
+  </>);
+}
